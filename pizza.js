7ece6efed7d412c76b298a07adc228755cf28a9c
pleundv <84378584+pleundv@users.noreply.github.com>
2021-06-09 11:48:56 +0200

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,26 @@
+const makePizza = function () {
+    console.log("Neem het deeg");
+    console.log("strijk de saus over het deeg");
+    console.log("Leg de mozarella op de rode saus");
+    console.log("leg de salami erboven op");
+    console.log("plaats de pizza in de oven")
+    console.log("enjoy your pizza!")
+}
+
+const makeSushi = function () {
+    console.log("pak een bamboe roller");
+    console.log("leg sushirijst op de roller druk aan");
+    console.log("Pak komkommer en zalm en leg het erop")
+    console.log("rol de sushirol");
+    console.log("snijd de sushi rol in kleine stukjes")
+    console.log("enjoy");
+}
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
+
